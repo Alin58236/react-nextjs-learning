c1d825b6a3be721a2e89b32b1d7b36c20b79029d
Alin Stoia <alin58236@gmail.com>
2025-11-24 03:20:50 +0200

    99 async

--- corp_comment/src/components/FeedbackList.tsx
+++ corp_comment/src/components/FeedbackList.tsx
@@ -1,30 +1,40 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
-
-
+import ErrorMessage from "./ErrorMessage";
 
 const FeedbackList = () => {
-
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
-    fetch('https://bytegrad.com/course-assets.projects/corpcomment/api/feedbacks')
-      .then((response) => response.json())
-      .then(data => {
-        setFeedbackItems(data.feedbacks)
+    fetch(
+      "https://bytegrad.com/course-assets.projects/corpcomment/api/feedbacks"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setErrorMessage(
+          error.message || "Something went wrong while fetching data."
+        );
+        console.error(errorMessage);
         setIsLoading(false);
       });
-      
-    
   }, []);
 
   return (
     <ol className="feedback-list">
-      
-      
       <>
         {/* //unstructured way with explicit props if in feedbackItem.tsx we had const FeedbackItem = (feedbackItem: feedbackItem)  */}
 
@@ -39,15 +49,14 @@ const FeedbackList = () => {
         ))} */}
       </>
 
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
 
-
-      {
-        isLoading ? <Spinner /> :
+      {isLoading ? (
+        <Spinner />
+      ) : (
         // structured way with single object prop const FeedbackItem = ({feedbackItem}: feedbackItemProps)
-        feedbackItems.map((item) => (
-          <FeedbackItem feedbackItem={item} />
-        ))
-      }
+        feedbackItems.map((item) => <FeedbackItem feedbackItem={item} />)
+      )}
     </ol>
   );
 };
