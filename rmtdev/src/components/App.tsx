e040fbdb697ca7b77f64ebea73f52333796164e5
Alin Stoia <alin58236@gmail.com>
2025-11-28 11:14:28 +0200

    sync

--- rmtdev/src/components/App.tsx
+++ rmtdev/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDebounce, useJobItems } from "../lib/hooks";
 import Background from "./Background";
 import Container from "./Container";
