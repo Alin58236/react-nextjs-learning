e8a51dccd2f0049d1fe624bcff1fee681fdd4813
Alin Stoia <alin58236@gmail.com>
2026-02-11 15:29:42 +0200

    created custom reusable css class for transition effects

--- project_evento/src/app/event/[slug]/page.tsx
+++ project_evento/src/app/event/[slug]/page.tsx
@@ -46,10 +46,14 @@ const EventPage = async (params: PageProps) => {
               })}
             </p>
 
-            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">{event.name}</H1>
-            <p className="text-white/75 whitespace-nowrap text-xl italic">Organized By {event.organizerName}</p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="text-white/75 whitespace-nowrap text-xl italic">
+              Organized By {event.organizerName}
+            </p>
 
-            <button className="bg-white/20 text-lg bg-blur capitalize text-white  w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-2 mt-5 lg:mt-auto hover:scale-105 active:scale-[1.02] transition">
+            <button className="bg-white/20 text-lg bg-blur capitalize text-white  w-[95vw] sm:w-full rounded-md border-white/10 border-2 py-2 mt-5 lg:mt-auto state-effect">
               Get Tickets
             </button>
           </div>
