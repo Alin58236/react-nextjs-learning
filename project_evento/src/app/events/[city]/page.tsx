1cbdfd5607aa265c26aeae1944ea5ede960ce1cb
Alin Stoia <alin58236@gmail.com>
2026-02-12 19:15:20 +0200

    Created suspense loading in events page

--- project_evento/src/app/events/[city]/page.tsx
+++ project_evento/src/app/events/[city]/page.tsx
@@ -1,33 +1,27 @@
+import Loading from "@/app/events/[city]/loading";
 import { EventsList } from "@/components/eventsList";
 import H1 from "@/components/h1";
 import { EventsPageProps, EventType } from "@/lib/types";
+import { Suspense } from "react";
 
 const EventsPage = async ({ params }: EventsPageProps) => {
   // we can access the city parameter from the URL using the params object passed to the page component. This allows us to dynamically render content based on the city specified in the URL. The way this is accessed is through the actual route given by the folder structure
   // eg. in this case it is events/city, so we can access the city parameter through params.city
 
   let paramList = await params;
-  
-  
   let city = await paramList.city.charAt(0).toUpperCase() + paramList.city.slice(1);
-  
-
-
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${paramList.city}`,
-  );
-
-  console.log(response);
-  const events: EventType[] = await response.json();
-
-  console.log(events);
+  console.log("Selected city: " + city);
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
 
       <H1 className="mb-28">{city === "All" ? "All Events" : `Events in ${city}`}</H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}> {// Suspense is used to handle the loading state of the EventsList component. While the data is being fetched, we can show a fallback UI (like a loading spinner) to indicate that the content is loading. Once the data is fetched, the EventsList component will be rendered with the fetched events.
+      }
+        <EventsList city={city} />
+      </Suspense>
+      
     </main>
   );
 };
--- project_evento/src/components/eventsList.tsx
+++ project_evento/src/components/eventsList.tsx
@@ -2,7 +2,19 @@ import { EventType } from "@/lib/types";
 import React from "react";
 import { EventCard } from "./eventCard";
 
-export const EventsList = ({ events }: { events: EventType[] }) => {
+export const EventsList = async ({ city }: { city: string }) => {
+
+  const slug = city.toLowerCase();
+  const searchString =  `https://bytegrad.com/course-assets/projects/evento/api/events?city=${slug}`;
+  console.log("Fetching events for city: " + searchString);
+
+  const response = await fetch(
+    searchString,
+  );
+
+  console.log(response);
+  const events: EventType[] = await response.json();
+
   return (
     <section className="flex flex-wrap gap-10 justify-center max-w-[1100px] px-[20px]">
       {events.map((event) => (
