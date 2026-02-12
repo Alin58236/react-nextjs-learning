a43cfa6a2fe246b4e62daf7745c858e314622230
Alin Stoia <alin58236@gmail.com>
2026-02-12 15:18:06 +0200

    Created loading page

---
+++ project_evento/src/app/event/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>Loading event...</div>
+  )
+}
+
+export default Loading
---
+++ project_evento/src/app/events/[city]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>Loading events in your city...</div>
+  )
+}
+
+export default Loading
