93ad87dc4e21608f9e6132aab4dd2f3bc70fcee3
Alin Stoia <alin58236@gmail.com>
2026-02-13 19:30:31 +0200

    245

--- project_evento/src/components/eventsList.tsx
+++ project_evento/src/components/eventsList.tsx
@@ -11,7 +11,8 @@ export const EventsList = async ({ city }: { city: string }) => {
   return (
     <section className="flex flex-wrap gap-10 justify-center max-w-[1100px] px-[20px]">
       {events.map((event) => (
-        <EventCard key={event.id} event={event} />
+        <EventCard key={event.id} event={event} />     
+        
       ))}
     </section>
   );
